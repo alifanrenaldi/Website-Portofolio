2210402aa56fb7b5780e8463759b7d09844acde7
alifanrenaldi <83490686+alifanrenaldi@users.noreply.github.com>
2023-12-15 01:34:08 +0700

    Add files via upload

---
+++ Pendidikan.php
@@ -0,0 +1,15 @@
+<?php 
+$connect = mysqli_connect("localhost", "root", "", "portofolio_native");
+
+function query($query) {
+
+    global $connect;
+    $result = mysqli_query($connect, "SELECT * FROM pendidikan");
+    $tampung = [];
+
+    while($baris = mysqli_fetch_assoc($result)) {
+        $tampung[] = $baris;
+    }
+    return $tampung;
+}
+?>
